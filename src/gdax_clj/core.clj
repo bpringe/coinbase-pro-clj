304e1002a8a9eab7deffad2dcea604e0d603dbee
Brandon Ringe <brandon.ringe@gmail.com>
2018-01-27 14:30:32 -0600

    Refactor request building to support different request methods more easily

--- src/gdax_clj/core.clj
+++ src/gdax_clj/core.clj
@@ -12,7 +12,7 @@
 ;;;;;;;;;;;; Configuration ;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def config {:api-base-url "https://api.gdax.com"
+(def config {:api-base-url "https://api-public.sandbox.gdax.com"
              :granularities {:1m 60
                              :5m 300
                              :15m 900
@@ -28,14 +28,24 @@
 ;;;;;;;;;;;; Request Building ;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn- build-request
-  [method path & [opts]]
-  (merge {:method (clojure.string/upper-case method)
-          :url (str (:api-base-url config)
-                    (if (clojure.string/starts-with? path "/") path (str "/" path)))
-          :as :json
-          :debug (:debug-requests config)
-          :headers {"Content-Type" "application/json"}}
+(defn- build-base-request
+  [method path]
+  {:method (clojure.string/upper-case method)
+   :url (str (:api-base-url config)
+          (if (clojure.string/starts-with? path "/") path (str "/" path)))
+   :accept :json
+   :as :json
+   :debug (:debug-requests config)})
+
+(defn- build-get-request
+  [path & [opts]]
+  (merge (build-base-request "GET" path)
+         opts))
+
+(defn- build-post-request
+  [path body & [opts]]
+  (merge (build-base-request "POST" path)
+         {:body body}
          opts))
 
 (defn- map->query-string
@@ -52,8 +62,6 @@
       #(str % 
         (if (clojure.string/includes? % "?") "&" "?") 
         (map->query-string paging-options)))))
-    
-  
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;; Authentication ;;;;;;;;;;;;;
@@ -79,10 +87,10 @@
 (defn- sign-request 
   [request]
   (let [timestamp (quot (System/currentTimeMillis) 1000)]
-    (update-in request [:headers] conj {"CB-ACCESS-KEY" (:api-key config)
-                                        "CB-ACCESS-SIGN" (create-signature timestamp request)
-                                        "CB-ACCESS-TIMESTAMP" timestamp
-                                        "CB-ACCESS-PASSPHRASE" (:api-passphrase config)})))
+    (update-in request [:headers] merge {"CB-ACCESS-KEY" (:api-key config)
+                                         "CB-ACCESS-SIGN" (create-signature timestamp request)
+                                         "CB-ACCESS-TIMESTAMP" timestamp
+                                         "CB-ACCESS-PASSPHRASE" (:api-passphrase config)})))
                        
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;; Public Endpoints ;;;;;;;;;;;;;;
@@ -90,82 +98,89 @@
 
 (defn get-time
   []
-  (http/request (build-request "get" "/time")))
+  (http/request (build-get-request "/time")))
 
 (defn get-products 
   []
-  (http/request (build-request "get" "/products")))
+  (http/request (build-get-request "/products")))
 
 (defn get-order-book
   ([product-id]
    (get-order-book product-id 1))
   ([product-id level]
    (->> (str "/products/" product-id "/book?level=" level)
-        (build-request "get")
+        build-get-request
         http/request)))
 
 (defn get-ticker
   [product-id]
   (->> (str "/products/" product-id "/ticker")
-       (build-request "get")
+       build-get-request
        http/request))
 
 (defn get-trades 
   [product-id]
   (->> (str "/products/" product-id "/trades")
-       (build-request "get")
+       build-get-request
        http/request))
 
 ;; Remember to use (t/today-at 00 00) to avoid sending time ahead of server time
 (defn get-historic-rates
   ([product-id]
    (->> "/products/eth-usd/candles"
-        (build-request "GET")
+        build-get-request
         http/request))
   ([product-id start end granularity]
    (->> (str "/products/" product-id "/candles?start="
          start "&end=" end "&granularity=" granularity)
-        (build-request "GET")
+        build-get-request
         http/request)))
       
 (defn get-product-stats
   [product-id]
   (->> (str "/products/" product-id "/stats")
-       (build-request "GET")
+       build-get-request
        http/request))
 
 (defn get-currencies
   []
-  (http/request (build-request "GET" "/currencies")))
+  (http/request (build-get-request "/currencies")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;; Private Endpoints ;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;; Accounts ;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn get-accounts 
   []
-  (-> (build-request "get" "/accounts")
-      sign-request
-      http/request))
+  (->> (build-get-request "/accounts")
+       sign-request
+       http/request))
 
 (defn get-account-by-id
   [account-id]
-  (->> (build-request "get" (str "/accounts/" account-id))
+  (->> (build-get-request (str "/accounts/" account-id))
        sign-request
        http/request))
 
 (defn get-account-history
   [account-id & [paging-options]]
-  (->> (build-request "get" (str "/accounts/" account-id "/ledger"))
+  (->> (build-get-request (str "/accounts/" account-id "/ledger"))
        (append-paging-options paging-options)
        sign-request
        http/request))
 
 (defn get-account-holds
   [account-id & [paging-options]]
-  (->> (build-request "get" (str "/accounts/" account-id "/holds"))
+  (->> (build-get-request (str "/accounts/" account-id "/holds"))
        (append-paging-options paging-options)
        sign-request
        http/request))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;; Orders ;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
