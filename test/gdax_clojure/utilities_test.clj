fac1252caef68c90cff76341875c6a095e773308
Brandon Ringe <brandon.ringe@gmail.com>
2018-06-11 18:43:34 -0500

    Finish utilities tests

--- test/gdax_clojure/utilities_test.clj
+++ test/gdax_clojure/utilities_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [gdax-clojure.utilities :refer :all]))
 
+(def default-options {:accept :json
+                      :as :json})
+
 (deftest test-edn->json
   (is (= "{\"hello\":\"world\"}"
          (edn->json {:hello "world"}))))
@@ -22,19 +25,51 @@
 (deftest test-build-base-request
   (let [method "GET"
         url "test url"
-        expected {:method method
-                  :url url
-                  :accept :json
-                  :as :json}]
+        expected (merge {:method method :url url} default-options)]
     (is (= expected (build-base-request method url)))))
 
 (deftest test-build-get-request
   (let [url "test url"
-        expected {:method "GET"
-                  :url url
-                  :accept :json
-                  :as :json}]
+        expected (merge {:method "GET" :url url} default-options)]
     (is (= expected (build-get-request url)))))
 
-  
+(deftest test-build-post-request
+  (let [url "test url"
+        body {:hello "world"}
+        options {:some "options"}
+        expected (merge {:method "POST"
+                         :url url
+                         :body "{\"hello\":\"world\"}"
+                         :content-type :json}
+                        default-options)]
+    (is (= expected (build-post-request url body))
+        "without options")
+    (is (= (merge expected options)
+           (build-post-request url body options))
+        "with options")))
+
+(deftest test-build-delete-request
+  (let [url "test url"
+        options {:some "options"}
+        expected (merge {:method "DELETE" :url url} default-options)]
+    (is (= expected (build-delete-request url))
+        "without options")
+    (is (= (merge expected options) (build-delete-request url options))
+        "with options")))
+                         
+(deftest test-map->query-string
+  (let [params {:a 1 :b 2 :c "hello"}
+        expected "a=1&b=2&c=hello"]
+    (is (= expected (map->query-string params))
+      "with non-empty map")
+    (is (= "" (map->query-string {}))
+      "with empty map")))
+                  
+(deftest test-append-query-params
+  (let [base-url "https://test"]
+    (is (= {:url (str base-url "?a=1&b=hello")} 
+           (append-query-params {:a 1 :b "hello"} {:url base-url}))
+      "without existing query params on url")
+    (is (= {:url (str base-url "?answer=42&message=hello")}
+           (append-query-params {:message "hello"} {:url (str base-url "?answer=42")})))))
 
